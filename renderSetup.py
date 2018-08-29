bbc7bb2f914f050c9a9126499adaff819b15af8d
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2018-08-29 17:05:24 +0900

    Update renderSetup.py

--- renderSetup.py
+++ renderSetup.py
@@ -12,3 +12,14 @@ import maya.app.renderSetup.model.renderLayer as renderLayer
 import maya.app.renderSetup.model.renderSetup as renderSetup
 import maya.app.renderSetup.model.connectionOverride as connectionOverride
 
+def getRenderLayerNameList:
+    layer_objects = renderSetup.instance().getRenderLayers()  # get layer object list
+    layer_names = []
+    for i in layer_objects:
+        layer_names.append(i.name())  # (object).name() return object name
+    return layer_names
+
+    # other way, to get renderSetupLayer node list with maya.cmds
+    # It returns the same result as the above function
+    layer_list = maya.cmds.ls(type='renderSetupLayer')
+    
