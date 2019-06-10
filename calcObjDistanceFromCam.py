3a146ad595f5a47f779af4de28627b6a265596e2
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-06-10 18:50:46 +0900

    Update calcObjDistanceFromCam.py

--- calcObjDistanceFromCam.py
+++ calcObjDistanceFromCam.py
@@ -1,5 +1,11 @@
+# calculate distance from camera using focal length and horizontal aperture
+
+from maya import cmds
 import math
-aov = 33
-tangent = math.tan(math.radians(aov/2))
-height = 0.08
-print height/tangent
+cam = 'renderCam'
+f_length = cmds.getAttr(cam+'.focalLength')
+h_aperture = cmds.getAttr(cam+'.horizontalFilmAperture')
+aov = (2 * math.atan((h_aperture * 25.4 * 0.5) / f_length)) * 57.29578
+tangent = math.tan(math.radians(aov / 2))
+obj_height = 0.085
+distance = obj_height/tangent
