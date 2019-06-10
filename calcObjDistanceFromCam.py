3c46817b5d3651daafa2cba8d459e588bd75a023
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-06-10 20:54:50 +0900

    Update calcObjDistanceFromCam.py

--- calcObjDistanceFromCam.py
+++ calcObjDistanceFromCam.py
@@ -6,7 +6,7 @@ import math
 cam = 'renderCam'
 f_length = cmds.getAttr(cam+'.focalLength')
 h_aperture = cmds.getAttr(cam+'.horizontalFilmAperture')
-aov = (2 * math.atan((h_aperture * 25.4 * 0.5) / f_length)) * 57.29578
+aov = math.degrees(2 * math.atan((h_aperture * 25.4 * 0.5) / f_length))
 tangent = math.tan(math.radians(aov / 2))
 obj_height = 0.085
 distance = obj_height/tangent
