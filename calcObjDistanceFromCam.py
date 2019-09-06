b392c6720bed91a983fbdb3cbc0e8397f9483e6e
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-09-06 12:27:14 +0900

    Update calcObjDistanceFromCam.py

--- calcObjDistanceFromCam.py
+++ calcObjDistanceFromCam.py
@@ -6,6 +6,12 @@ import math
 cam = 'renderCam'
 obj_height = 0.085
 
+# if get obj_height from bounding box size
+obj_name = 'target_name'
+min_val = cmds.getAttr(obj_name + '.boundingBoxMin')
+max_val = cmds.getAttr(obj_name + '.boundingBoxMax')
+obj_height = abs(min_val[0][1]) + abs(max_val[0][1])
+
 f_length = cmds.getAttr(cam+'.focalLength')
 h_aperture = cmds.getAttr(cam+'.horizontalFilmAperture')
 aov = math.degrees(2 * math.atan((h_aperture * 25.4 * 0.5) / f_length))
