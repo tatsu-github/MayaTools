f9ad35aa0123a1e34cee5f8cfa7ff1eb1e59a4c1
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-09-06 12:14:44 +0900

    Update calcObjDistanceFromCam.py

--- calcObjDistanceFromCam.py
+++ calcObjDistanceFromCam.py
@@ -4,9 +4,10 @@ from maya import cmds
 import math
 
 cam = 'renderCam'
+obj_height = 0.085
+
 f_length = cmds.getAttr(cam+'.focalLength')
 h_aperture = cmds.getAttr(cam+'.horizontalFilmAperture')
 aov = math.degrees(2 * math.atan((h_aperture * 25.4 * 0.5) / f_length))
 tangent = math.tan(math.radians(aov / 2))
-obj_height = 0.085
 distance = obj_height/tangent
