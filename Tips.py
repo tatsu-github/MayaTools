bd84cca029e511408dc899a0168280787528a351
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-10-16 18:47:23 +0900

    Update Tips.py

--- Tips.py
+++ Tips.py
@@ -1,2 +1,6 @@
 # print attrs from selected node
 for i in maya.cmds.listAttr(http://maya.cmds.ls(sl=True)[0]):print(i)
+
+# get file name without extention
+from maya import cmds
+file_name = (cmds.file(q=True, sn=True, shn=True)).split('.')[0]
