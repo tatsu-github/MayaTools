a5a48f8ab388137423b5bcf9599e97a2d4f3cdcf
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-10-16 18:50:04 +0900

    Update Tips.py

--- Tips.py
+++ Tips.py
@@ -4,3 +4,6 @@ for i in maya.cmds.listAttr(http://maya.cmds.ls(sl=True)[0]):print(i)
 # get file name without extention
 from maya import cmds
 file_name = (cmds.file(q=True, sn=True, shn=True)).split('.')[0]
+
+# change working units to Centimeter
+cmds.currentUnit(linear='cm')
