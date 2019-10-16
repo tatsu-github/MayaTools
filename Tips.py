4f1ae7876979c45ce900d1c47d09d662d13f0c74
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-10-16 18:50:54 +0900

    Update Tips.py

--- Tips.py
+++ Tips.py
@@ -7,3 +7,11 @@ file_name = (cmds.file(q=True, sn=True, shn=True)).split('.')[0]
 
 # change working units to Centimeter
 cmds.currentUnit(linear='cm')
+
+# print Maya plug-in path
+from maya import cmds, mel
+path = mel.eval('getenv "MAYA_PLUG_IN_PATH"')
+path_list = path.split(':')
+for i in path_list:
+    print i
+    
