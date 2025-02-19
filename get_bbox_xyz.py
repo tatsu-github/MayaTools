a85c5a7f4f9cfcda4ef8fcfbb6a457d6c60a22a3
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2025-02-19 15:01:54 +0900

    Create get_bbox_xyz.py

---
+++ get_bbox_xyz.py
@@ -0,0 +1,9 @@
+from maya import cmds
+
+def get_bbox_xyz():
+    bbox = cmds.exactWorldBoundingBox(cmds.ls(sl=True, type='transform')[0])
+    xmin, ymin, zmin, xmax, ymax, zmax = bbox[0], bbox[1], bbox[2], bbox[3], bbox[4], bbox[5]
+    width = xmax - xmin
+    height = ymax - ymin
+    depth = zmax - zmin
+  
